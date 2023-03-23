1c7d866eb5acf70d6c64204d1646b1d25d87f44b
Andy Molinaro <andrew.molinaro916@gmail.com>
2023-03-23 18:56:04 -0500

    Updates: Created add, subtract, multiply, divide functions. Added Operate function. Added First Number, Second Number and Operator variables.

---
+++ script.js
@@ -0,0 +1,38 @@
+const add = function (a, b) {
+    return a+b;
+}
+
+
+const subtract = function (a, b) {
+    return a-b;
+}
+
+const multiply = function (a, b) {
+    return a*b;
+}
+
+const divide = function (a, b) {
+    if (b == 0) {
+        return "NaN";
+    }
+    return a/b;
+}
+
+var firstNumber;
+var secondNumber;
+var Operator; 
+
+const operate = function (operator, firstNumber, secondNumber) {
+    if (operator == "+") {
+        return add(firstNumber, secondNumber);
+    }
+    else if (operator == "-") {
+        return subtract(firstNumber, secondNumber);
+    }
+    else if (operator == "*") {
+        return multiply(firstNumber, secondNumber);
+    }
+    else if (operator == "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
